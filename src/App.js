1dbfb791246615343634f204dea65c8e1be3ca23
Ivan_Bahena <Ivan.BahenaLedezma@gmail.com>
2020-11-15 00:08:01 +0100

    Components P1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
 import './App.css';
+import { RIDES } from './shared/rides'
+import List from "./components/RideComponent";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+      <header className="App-header">Paris Taxi Fare</header>
+      <body> 
+        <div> 
+          <List RIDES={RIDES}/>
+        </div>
+      </body>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/RideComponent.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import moment from "moment";
+import 'moment-duration-format';
+import { Media } from 'reactstrap';
+
+function List(props) {
+  const rides = props.RIDES;
+  var i_rides = Array.from({length: rides.length}, (x, i) => i);
+
+  function message_alert(i_ride){
+    //ride duration, in the hh:mm:ss format as well as the end time of the ride.
+    var duration_alert=moment.duration(rides[i_ride].duration,"seconds").format("hh:mm:ss");
+    var end_time_alert=moment.utc(Date.parse(rides[i_ride].startTime)+rides[i_ride].duration*1000).format();
+    return duration_alert+'-'+end_time_alert
+  };
+  
+  function price(i_ride){
+    var additional_price;
+    var time_in_hr_for_add_price=moment.utc(rides[i_ride].startTime).format("HH");
+   if (time_in_hr_for_add_price>16 && time_in_hr_for_add_price<19){ //1 EURadditional for busy periods between 4PM and 7PM
+   additional_price=1;
+   }else if (time_in_hr_for_add_price>20 || time_in_hr_for_add_price<6){//0.50 EUR additional EUR between 8PM and 6AM
+    additional_price=0.5;
+   } else{
+    additional_price=0;
+   };
+    return 1 + rides[i_ride].distance*2.50+additional_price;
+  }
+  
+  
+    
+  function clicked(i_button){
+    document.getElementsByTagName("span")[i_button].innerHTML = "Clicked";
+  };
+
+  function background_red(i_box){
+    if (rides[i_box].id>2) {
+      document.getElementsByTagName("box")[i_box].style.backgroundColor = "red";
+    } ;
+  };
+  
+
+  return (
+    <ul>{i_rides.map((i_ride) => 
+      <div >
+        {(rides[i_ride].id>2)? <div id="box" style ={{ backgroundColor : 'red' }}><h2> ride ID = {rides[i_ride].id} <span></span></h2>  price= {price(i_ride)} EUR
+        <button id="button" onClick={() => clicked(i_ride) & alert(message_alert(i_ride)) } >
+        Click
+        </button>
+        <div>
+    </div>
+        </div> : (<div id="box" ><h2> ride ID = {rides[i_ride].id} <span></span></h2> price= {price(i_ride)} EUR
+        <button id="button" onClick={() => clicked(i_ride) & alert(message_alert(i_ride)) }>
+        Click
+        </button>
+        <div>
+    </div>
+        </div>) } 
+      </div>)}
+      
+    </ul>
+  );
+}
+
+export default List
+
